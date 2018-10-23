91030dc8e50b8a3b85544c6e0589923f8967d1f1
Ben Shurts <benjaminshurts@gmail.com>
2018-10-23 11:13:39 -0600

    js stuff

--- mainjs.js
+++ mainjs.js
@@ -1,10 +1,13 @@
-import { films } from './assets/films'
+import { films } from './films'
 
-
+let film = films();
 // stuff below this makes stuff happen
 
 let cardList = document.querySelector("#filmList");
 
-films.forEach((fimls) => {
+films.forEach((films) => {
     console.log(film.title)
+    let filmList = document.createElement("li");
+    
 })
+
