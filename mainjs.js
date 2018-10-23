83bd3ae9ecf3242479f2ce46dacc99f07e5d243e
Ben Shurts <benjaminshurts@gmail.com>
2018-10-22 19:53:37 -0600

    more swapi

--- mainjs.js
+++ mainjs.js
@@ -5,4 +5,6 @@ import { species } from './assets/js/species'
 import { starships } from './assets/js/starships'
 import { vehicles } from './assets/js/vehicles'
 
+// stuff below this makes stuff happen
+
 let cardList = document.querySelector("card-list");
