44b910253fe656955568beb69a71185118287b32
Ben Shurts <benjaminshurts@gmail.com>
2018-11-01 11:08:57 -0600

    api sorting

--- mainjs.js
+++ mainjs.js
@@ -1,64 +1,13 @@
-//import { films } from './films'
+import { senateData } from "./senators.js"
 
-//vars
+const senators = senateData.results[0].members
 
-// let luke = document.querySelector('#luke')
-// let crawl = document.querySelector('#crawl')
-// let movieList = document.querySelector('#movieList')
-// let characters = document.querySelector('#characters')
-// //starter crawl
-// fetch('https://swapi.co/api/films/1')
-//   .then(function(response) {
-//     return response.json();
-//   })
-//   .then(function(myJson) {
-//     console.log(myJson);
-//     //console.log(myJson.);
-//     luke.textContent = myJson.title
-//     crawl.textContent = myJson.opening_crawl
-//   });
+console.log(senators.length)
 
-// //list of movies
-// fetch('https://swapi.co/api/films/')
-//   .then(function(response){
-//     return response.json();
-//   })
-//   //print list of stuff within api object characters
-//   .then(function(myJson) {
-//     myJson.results.forEach( (film) => {
-//       film.characters.forEach( (character) => {
-//         console.log(character)
-//       })
-//     })
-//   })
+// const mostVotesEver = senators.reduce((acc, senator) => acc + senator.total_votes, 0)
+// console.log(mostVotesEver)
 
-//real list for movies
-// fetch('https://swapi.co/api/films/')
-//   .then(function(response){
-//     return response.json()
-//   })
-// //print list of stuff within api object
-// .then(function(myJson) {
-//   myJson.results.forEach( (film) => {    
-//     let listItem = document.createElement('li')
-//     listItem.textContent = film.title
-//     movieList.appendChild(listItem)
-
-//     console.log(film.title)
-//   })  
-// })
-
-// fetch('https://swapi.co/api/people/')
-//   .then(function(response){
-//     return response.json()
-//     //console.log(Json)
-//   })
-// //print list of stuff within api object
-// .then(function(myJson) {
-//   myJson.results.forEach( (people) => {    
-//     let listItem = document.createElement('li')
-//     listItem.textContent = people.name
-//     movieList.appendChild(listItem)
-//     console.log(people.name)
-//   })
-// })
+const mostVotesEver = senators.reduce((acc, senator) => {
+  return(acc.total_votes || 0) > senator.total_votes ? acc : senator
+},{})
+console.log("Most votes cast by any senator was " + mostVotesEver)
