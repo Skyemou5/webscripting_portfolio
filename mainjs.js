07fe4c8af367102efe85cde5ad91eca3745471e3
Ben Shurts <benjaminshurts@gmail.com>
2018-10-31 23:31:49 -0600

    Starting to rebuild everything

--- mainjs.js
+++ mainjs.js
@@ -2,21 +2,21 @@
 
 //vars
 
-let luke = document.querySelector('#luke')
-let crawl = document.querySelector('#crawl')
-let movieList = document.querySelector('#movieList')
-let characters = document.querySelector('#characters')
-//starter crawl
-fetch('https://swapi.co/api/films/1')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(myJson);
-    //console.log(myJson.);
-    luke.textContent = myJson.title
-    crawl.textContent = myJson.opening_crawl
-  });
+// let luke = document.querySelector('#luke')
+// let crawl = document.querySelector('#crawl')
+// let movieList = document.querySelector('#movieList')
+// let characters = document.querySelector('#characters')
+// //starter crawl
+// fetch('https://swapi.co/api/films/1')
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(myJson) {
+//     console.log(myJson);
+//     //console.log(myJson.);
+//     luke.textContent = myJson.title
+//     crawl.textContent = myJson.opening_crawl
+//   });
 
 // //list of movies
 // fetch('https://swapi.co/api/films/')
@@ -33,32 +33,32 @@ fetch('https://swapi.co/api/films/1')
 //   })
 
 //real list for movies
-fetch('https://swapi.co/api/films/')
-  .then(function(response){
-    return response.json();
-  })
-//print list of stuff within api object
-.then(function(myJson) {
-  myJson.results.forEach( (film) => {    
-    let listItem = document.createElement('li');
-    listItem.textContent = film.title
-    movieList.appendChild(listItem)
+// fetch('https://swapi.co/api/films/')
+//   .then(function(response){
+//     return response.json()
+//   })
+// //print list of stuff within api object
+// .then(function(myJson) {
+//   myJson.results.forEach( (film) => {    
+//     let listItem = document.createElement('li')
+//     listItem.textContent = film.title
+//     movieList.appendChild(listItem)
 
-    console.log(film.title)
-  })  
-})
+//     console.log(film.title)
+//   })  
+// })
 
-fetch('https://swapi.co/api/people/')
-  .then(function(response){
-    return response.json();
-    //console.log(Json)
-  })
-//print list of stuff within api object
-.then(function(myJson) {
-  myJson.results.forEach( (people) => {    
-    let listItem = document.createElement('li');
-    listItem.textContent = people.name
-    movieList.appendChild(listItem)
-    console.log(people.name)
-  })
-})
+// fetch('https://swapi.co/api/people/')
+//   .then(function(response){
+//     return response.json()
+//     //console.log(Json)
+//   })
+// //print list of stuff within api object
+// .then(function(myJson) {
+//   myJson.results.forEach( (people) => {    
+//     let listItem = document.createElement('li')
+//     listItem.textContent = people.name
+//     movieList.appendChild(listItem)
+//     console.log(people.name)
+//   })
+// })
