b34927de98d5159beca2b52ce0e1e1babb9dea99
Ben Shurts <benjaminshurts@gmail.com>
2018-11-02 15:32:13 -0600

    css stuff

--- mainjs.js
+++ mainjs.js
@@ -1,4 +1,17 @@
 import { senateData } from "./senators.js"
+//manipulate css vars
+const colorInput = document.querySelector('input[type=color]')
+const colorButton = document.querySelector('button')
+const colorVariable = "--yellow"
+
+colorButton.addEventListener('click', _ => {
+  document.documentElement.style.setProperty(colorVariable,'green')
+})
+
+colorInput.addEventListener('change', e => {
+  console.log(e)
+})
+
 
 const senators = senateData.results[0].members
 
@@ -11,3 +24,6 @@ const mostVotesEver = senators.reduce((acc, senator) => {
   return(acc.total_votes || 0) > senator.total_votes ? acc : senator
 },{})
 console.log("Most votes cast by any senator was " + mostVotesEver)
+
+// nasa api key e1k1ZUbZkI2cFnAUCjaaipqc4wKlH0VNPF0wXUwn
+// https://api.nasa.gov/planetary/apod?api_key=e1k1ZUbZkI2cFnAUCjaaipqc4wKlH0VNPF0wXUwn
