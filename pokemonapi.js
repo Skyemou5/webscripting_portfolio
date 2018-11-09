d350d6e60d98b46e110b65b89be2bfafcddb991c
Ben Shurts <benjaminshurts@gmail.com>
2018-11-09 09:34:24 -0700

    pokemon api stuff

--- pokemonapi.js
+++ pokemonapi.js
@@ -1,9 +1,12 @@
-const getPoke = () => {
-    return fetch('https://pokeapi.co/api/v2/pokemon')
-        .then(res => res.json())
-        .then(pokemon => console.log(pokemon))
-}
+// const getPoke = () => {
+//     return fetch('https://pokeapi.co/api/v2/pokemon')
+//         .then(res => res.json())
+//         .then(pokemon => console.log(pokemon))
+// }
 
-const newPokemon = 
+// console.log(pokemon)
 
-console.log(pokemon)
+var card = document.querySelector('.card');
+card.addEventListener( 'click', function() {
+  card.classList.toggle('is-flipped');
+});
